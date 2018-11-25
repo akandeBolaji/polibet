a15937686ff618afe7ba20759efa63d5e61d8d44
akandeBolaji <akandbolajidanielofficial@gmail.com>
2018-11-25 02:22:29 +0100

    added https middleware redo

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,11 +35,11 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            'App\Http\Middleware\HttpsProtocol'
+            \App\Http\Middleware\HttpsProtocol
         ],
 
         'api' => [
-            'App\Http\Middleware\HttpsProtocol',
+             \App\Http\Middleware\HttpsProtocol,
             'throttle:60,1',
             'bindings',
         ],
