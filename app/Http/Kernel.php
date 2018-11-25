eb74e283a28d37b68b64a7b0f0a6c0f74bfad18e
akandeBolaji <akandbolajidanielofficial@gmail.com>
2018-11-25 14:15:31 +0100

    another commit

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,11 +35,10 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            //\App\Http\Middleware\HttpsProtocol::class
+            \App\Http\Middleware\HttpsProtocol::class
         ],
 
         'api' => [
-           // \App\Http\Middleware\HttpsProtocol::class,
             'throttle:60,1',
             'bindings',
         ],
