1d27f74840df90df88dcca79950d79562f5b884d
akandeBolaji <akandbolajidanielofficial@gmail.com>
2018-11-27 19:29:31 +0100

    another commit

--- resources/views/mail/account/activation.blade.php
+++ resources/views/mail/account/activation.blade.php
@@ -8,6 +8,15 @@ We would love you to click on the button below to verify your account and start 
 Verify Account
 @endcomponent
 
+@component('mail::panel')
+If you're having trouble clicking the "Verify ACccount" button directly, copy and paste the URL below into your web browser.
+@endcomponent
+
+@component('mail::panel')
+{{ $url }}
+@endcomponent
+
+
 Regards,<br>
 {{ config('app.name') }}
 @endcomponent
