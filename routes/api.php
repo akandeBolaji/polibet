bfdc87ce3ea02ca37c18fea6ba5d1f935b8be1d4
akandeBolaji <akandbolajidanielofficial@gmail.com>
2019-09-25 01:12:13 +0100

    little change

--- routes/api.php
+++ routes/api.php
@@ -16,8 +16,8 @@ use Illuminate\Http\Request;
 //Route::middleware('auth:api')->get('/user', function (Request $request) {
    // return $request->user();
 //});
+Route::post('ussd','UssdController@index');
 
-Route::post('/ussd', 'UssdController@index');
 
 Route::group(['prefix' => 'auth'], function () {
     Route::post('/login','AuthController@authenticate');
